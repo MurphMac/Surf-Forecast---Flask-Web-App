4a599f0ffa58688eb9ecaf4e22bdd624d2e4d8d8
MurphyMc01 <87953140+MurphyMc01@users.noreply.github.com>
2024-06-05 14:16:46 +1200

    Image of Surf Data on /data

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
 import pandas as pd
+import matplotlib.pyplot as plt
+import io
+import base64
 
 app = Flask(__name__)
 
@@ -8,10 +11,23 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/test')
-def hello():
-    df = pd.read_csv('C:\\Users\\cuteb\\Downloads\\mov-line-graph-data (4).zip\\swan_gfs_nz-ncanterb_v3.0_rb70bv50.csv')
-    return df.to_html()
+@app.route('/data')
+
+def data():
+    df = pd.read_csv('data\swan_gfs_nz-ncanterb_v3.0_rb70bv50.csv')
+
+    #return df.to_html()
+
+    #set up image
+    img = io.BytesIO()
+    #plot data
+    df.plot()
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+    return render_template("data.html", plot_url=plot_url)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
