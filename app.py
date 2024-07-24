9788262103a846e1a69250ca1f35929041c18b9b
MurphyMc01 <87953140+MurphyMc01@users.noreply.github.com>
2024-07-24 19:05:44 +1200

    Graph adjustments and horizontally aligned.

--- app.py
+++ app.py
@@ -8,12 +8,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/data')
+@app.route('/graph1')
 
-def graphs():
+def graphs1():
     # source data
     df_swell = pd.read_csv('data\swan_gfs_nz-ncanterb_v3.0_rb70bv50.csv')
-    # df_wind = pd.read_csv('data\gfs025_sub_v1.0.csv')
+    #Drop last 12 rows
+    df_swell.drop(index=df_swell.index[-12:], axis=0, inplace=True)
 
     # Create a list of tuples containing time and swell size
     swell_data = list(zip(df_swell['time:Pacific/Auckland'], df_swell['hs:m']))
@@ -28,11 +29,30 @@ def graphs():
         #Add only time of day to labels
         labels.append(times[11:16])
 
-    for i in range(12):
-        labels.pop()
-
     #return html with data
-    return render_template('data.html', labels=labels, values1=values1, values2=values2)
+    return render_template('graph1.html', labels=labels, values1=values1, values2=values2)
+
+@app.route('/graph2')
+
+def graphs2():
+    # source data
+    df_wind = pd.read_csv('data\gfs025_sub_v1.0.csv')
+    #Drop last 12 rows
+    df_wind.drop(index=df_wind.index[-12:], axis=0, inplace=True)
+
+    #Get Wind speed
+    values1 = list(df_wind['wind_speed_at_10m_above_ground_level:kt'])
+
+    #Get Gust speed
+    values2 = list(df_wind['wind_speed_of_gust_at_10m_above_ground_level:kt'])
+    
+    labels = []
+    dates = df_wind['time:Pacific/Auckland']
+    for times in dates:
+        labels.append(times[11:16])
+
+    return render_template('graph2.html', labels=labels, values1=values1, values2=values2)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
