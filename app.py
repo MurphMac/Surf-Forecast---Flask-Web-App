20615f2edbece4047746c76f4ed4206367587c8c
MurphyMc01 <87953140+MurphyMc01@users.noreply.github.com>
2024-09-02 10:42:12 +1200

    Initial format and data for Time and Location table

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 import pandas as pd
+from datetime import datetime, timedelta
 import calendar
+import sqlite3
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -10,20 +12,34 @@ app.config['SECRET_KEY'] = 'testkey'
 
 db = SQLAlchemy(app)
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
-
-with app.app_context():
-    db.create_all()
-
 # source data
-df_wind = pd.read_csv('data\gfs025_sub_v1.0.csv')
-#Drop last 12 rows
-df_wind.drop(index=df_wind.index[-12:], axis=0, inplace=True)
-#Get dates
-dates = df_wind['time:Pacific/Auckland']
+df_tide = pd.read_csv(r'data\results.csv', header=7)
+
+# Create a list of tuples containing time and tide height
+tide_data = list(zip(df_tide['TIME'], df_tide['VALUE']))
+
+dates = [row[0] for row in tide_data]
+
+# Convert dates into correct format and order
+for i in range(len(dates)):
+    #Convert to datetime object
+    dt = datetime.strptime(dates[i], "%Y-%m-%dT%H:%M:%SZ")
+    #Subtract 12 hours
+    dt -= timedelta(hours=12)
+    #Convert back to string
+    dates[i] = dt.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+
+# Clear table
+cursor.execute("DELETE FROM time")
+
+for date in dates:
+    cursor.execute("INSERT INTO time (date_time) VALUES (?)", (date,))
+
+conn.commit()
+conn.close()
 
 @app.route('/')
 
@@ -35,18 +51,22 @@ def home():
         year, month, day = int(date[0:4]), int(date[5:7]), int(date[8:10])
         #Add weekday value to list
         weekday = calendar.day_name[calendar.weekday(year, month, day)]
-        days.append(weekday)
+        weekday = str(weekday)[0:3]
+        #Get name of month
+        month_name = str(calendar.month_name[month])[0:3]
+        days.append(f"{weekday} {day} {month_name}")
         #Remove duplicates
         days = list(dict.fromkeys(days))
 
+
     #Assign value to each day on the graph
-    day1=(days[0])[0:3]
-    day2=(days[1])[0:3]
-    day3=(days[2])[0:3]
-    day4=(days[3])[0:3]
-    day5=(days[4])[0:3]
-    day6=(days[5])[0:3]
-    day7=(days[6])[0:3]
+    day1=(days[0])
+    day2=(days[1])
+    day3=(days[2])
+    day4=(days[3])
+    day5=(days[4])
+    day6=(days[5])
+    day7=(days[6])
 
     return render_template("home.html", day1=day1, day2=day2, day3=day3, day4=day4, day5=day5, day6=day6, day7=day7)
 
@@ -63,12 +83,18 @@ def register():
 @app.route('/graph1')
 
 def graphs1():
+    # source data
+    df_wind = pd.read_csv(r'data\gfs025_sub_v1.0.csv')
+    #Drop last 12 rows
+    df_wind.drop(index=df_wind.index[-12:], axis=0, inplace=True)
+    #Get dates
+    dates = df_wind['time:Pacific/Auckland']
+
     #Get Wind speed
     values1 = list(round(x, 2) for x in df_wind['wind_speed_at_10m_above_ground_level:kt'])
-
     #Get Gust speed
     values2 = list(round(x, 2) for x in df_wind['wind_speed_of_gust_at_10m_above_ground_level:kt'])
-    
+
     #Get Labels
     labels = []
     #dates = df_wind['time:Pacific/Auckland']
@@ -81,7 +107,7 @@ def graphs1():
 
 def graphs2():
     # source data
-    df_swell = pd.read_csv('data\swan_gfs_nz-ncanterb_v3.0_rb70bv50.csv')
+    df_swell = pd.read_csv(r'data\swan_gfs_nz-ncanterb_v3.0_rb70bv50.csv')
     #Drop last 12 rows
     df_swell.drop(index=df_swell.index[-12:], axis=0, inplace=True)
 
@@ -101,8 +127,17 @@ def graphs2():
     #return html with data
     return render_template('graph2.html', labels=labels, values1=values1, values2=values2)
 
-with app.app_context():
-    db.create_all()
+@app.route('/graph3')
+
+def graphs3():
+    values1 = [row[1] for row in tide_data]
+    labels = []
+
+    for times in dates:
+        #Add only time of day to labels
+        labels.append(times[11:16])
+
+    return render_template('graph3.html', labels=labels, values1=values1)
 
 if __name__ == "__main__":
     app.run(debug=True)
